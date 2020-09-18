1828a83e8a911572cf4806ee47cd59ddc0616cde
kapilmatre9893 <kapil.matre@lemosys.com>
2020-09-18 17:11:05 +0530

    Updates

---
+++ precache-manifest.0664877498145264db61ce0a69fae0eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaa6a38ba366895941399f01a707472",
+    "url": "/reacjWebsite/index.html"
+  },
+  {
+    "revision": "fee3e641dd1095dd835d",
+    "url": "/reacjWebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "29e89de8bfdb491adf32",
+    "url": "/reacjWebsite/static/css/main.26e4e16d.chunk.css"
+  },
+  {
+    "revision": "fee3e641dd1095dd835d",
+    "url": "/reacjWebsite/static/js/2.ae10a92e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reacjWebsite/static/js/2.ae10a92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e89de8bfdb491adf32",
+    "url": "/reacjWebsite/static/js/main.9af9d962.chunk.js"
+  },
+  {
+    "revision": "0c4e9ad91116b9543be3",
+    "url": "/reacjWebsite/static/js/runtime-main.9bcac9bb.js"
+  },
+  {
+    "revision": "e8e8d2c9ebd9ff4162c56a66cc7edfce",
+    "url": "/reacjWebsite/static/media/app.e8e8d2c9.png"
+  },
+  {
+    "revision": "89d1bfb97e7fa0a518e016c01d032821",
+    "url": "/reacjWebsite/static/media/digi.89d1bfb9.jpeg"
+  },
+  {
+    "revision": "75b14e0d2c0a03b947e9ce6342c3c898",
+    "url": "/reacjWebsite/static/media/home.75b14e0d.png"
+  },
+  {
+    "revision": "17f20e9dde2243f0845cce21e622728e",
+    "url": "/reacjWebsite/static/media/home2.17f20e9d.png"
+  },
+  {
+    "revision": "c0f0d9a50319d17b23189a1364ae6273",
+    "url": "/reacjWebsite/static/media/marking.c0f0d9a5.jpeg"
+  },
+  {
+    "revision": "c40768c1ccd36bf301c092baacebd5e9",
+    "url": "/reacjWebsite/static/media/software.c40768c1.jpg"
+  },
+  {
+    "revision": "5670260120f7f7bdd2f35793af91030b",
+    "url": "/reacjWebsite/static/media/wdevelp.56702601.jpeg"
+  },
+  {
+    "revision": "78cb2d7562471ff8937512f3b4490221",
+    "url": "/reacjWebsite/static/media/web degine.78cb2d75.jpeg"
+  }
+]);
