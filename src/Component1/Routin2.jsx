7290d051663f55bde6eadb80a50c52a2c60070c7
kapilmatre9893 <kapil.matre@lemosys.com>
2020-09-23 16:43:17 +0530

    Save before ejecting

--- src/Component1/Routin2.jsx
+++ src/Component1/Routin2.jsx
@@ -10,17 +10,10 @@ import Webg from '../wEB/wEB';
 
     return(
         <>
-    
-
          <Switch>
          <Route exact path='/Login' component={Login} />
          <Route exact path='/Sig' component={Signup} />
          <Route exact path='/Web' component={Webg} />
-         {/* <Route exact  path='/home' component={Home} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/service' component={Service} />
-      <Route exact path='/team' component={Team} />
-      <Route exact path='/contact' component={Contact} /> */}
          </Switch> 
         </>
     );
